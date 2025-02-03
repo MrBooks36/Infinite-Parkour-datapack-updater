42ea1cf4c18cfb9d13584f4e239c1696a1eafa34
MrBooks36 <mrbooks36@outlook.com>
2025-02-04 09:54:48 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -2,31 +2,39 @@ import os
 from git import Repo
 import shutil
 
-#get username
-username = os.getlogin()
+
 #def paths
-path = f'C:/Users/{username}/AppData/Roaming/.minecraft/saves/infinite-parkour-alpha-v0.1.1/datapacks/Infinite-Parkour-datapack'
+if os.path.exists('config.txt'):
+  fd = os.open("config.txt", os.O_RDONLY) 
+  path = os.read(fd, 50) 
+  print(path)
+  p = (f'{path}/saves/infinite-parkour-alpha-v0.1.2/datapacks/Infinite-Parkour-datapack')
+else:
+  username = os.getlogin()
+  p = (f'C:/Users/{username}/AppData/Roaming/.minecraft/saves/infinite-parkour-alpha-v0.1.2/datapacks/Infinite-Parkour-datapack')
+  print(p)
+
 repo_url = 'https://github.com/Big-Con-Gaming/Infinite-Parkour-datapack'
 
-#give access to pain in the but to remove files 
+#give access to pain in the butt to remove files 
 try:
- os.chmod(f'{path}\.git\objects\pack\pack-bf62a5020e26a1196cbf38be806c5194d4ff52b8.idx', 0o777)
+ os.chmod(f'{p}\.git\objects\pack\pack-bf62a5020e26a1196cbf38be806c5194d4ff52b8.idx', 0o777)
 except:
    print('1')
 try:
- os.chmod(f'{path}\.git\objects\pack\pack-bf62a5020e26a1196cbf38be806c5194d4ff52b8.pack', 0o777)
+ os.chmod(f'{p}\.git\objects\pack\pack-bf62a5020e26a1196cbf38be806c5194d4ff52b8.pack', 0o777)
 except:
    print('2')
 try:
- os.chmod(f'{path}\.git\objects\pack\pack-bf62a5020e26a1196cbf38be806c5194d4ff52b8.rev', 0o777)
+ os.chmod(f'{p}\.git\objects\pack\pack-bf62a5020e26a1196cbf38be806c5194d4ff52b8.rev', 0o777)
 except:
    print('3')
 
-if os.path.exists(path):
-    shutil.rmtree(path)
+if os.path.exists(p):
+    shutil.rmtree(p)
     print(f"Old pack has been deleted.")
 print('installing new pack')
-repo = Repo.clone_from(repo_url, f"{path}")
-os.chdir(path)
-os.system(f'{path}/autobuild.bat')
+repo = Repo.clone_from(repo_url, f"{p}")
+os.chdir(p)
+os.system(f'{p}/autobuild.bat')
 input('press enter to end')
