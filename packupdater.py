72bf654708c18a444ad7f67d3144920fc796ecd2
MrBooks36 <mrbooks36@outlook.com>
2025-02-04 12:55:58 +1000

    Add files via upload

---
+++ packupdater.py
@@ -0,0 +1,61 @@
+try:
+ from os import getlogin, path, chmod, chdir, system
+ from git import Repo
+ from shutil import rmtree
+ username = getlogin()
+ print(f'Hi {username}')
+
+ def startup():
+  from win32com.client import Dispatch
+  target = path.abspath(__file__)
+  shell = Dispatch("WScript.Shell")
+  shortcut = shell.CreateShortCut(f'C:/Users/{username}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/packupdater.lnk')
+  shortcut.Targetpath = target
+  shortcut.IconLocation = target
+  shortcut.save()
+
+ response = input("Do you want to open updater on PC startup? Y/n ").strip().lower()
+
+ if response == 'y':
+    print("Adding to startup ")
+    startup
+ elif response == 'n':
+   print("ok")
+ else:
+    print("Invalid input. Please enter 'y' or 'n'.")
+ #def paths
+ if path.exists('config.txt'):
+   with open('config.txt', 'r') as file:
+    path = file.read()
+    p = (f'{path}/saves/infinite-parkour-alpha-v0.1.2/datapacks/Infinite-Parkour-datapack')
+    print(f'Folder path at\n{p}')
+ else:
+   p = (f'C:/Users/{username}/AppData/Roaming/.minecraft/saves/infinite-parkour-alpha-v0.1.2/datapacks/Infinite-Parkour-datapack')
+   print(f'Folder path at\n{p}')
+
+ repo_url = 'https://github.com/Big-Con-Gaming/Infinite-Parkour-datapack'
+
+ #give access to pain in the butt to remove files 
+ try:
+  chmod(f'{p}/.git/objects/pack', 0o777)
+  print('removed pain in the butt files ')
+ except:
+   print('removed pain in the butt files ')
+
+ try:
+  if path.exists(p):
+     rmtree(p)
+     print(f"Old pack has been deleted.")
+     print('installing new pack')
+ except:
+   print("Your config.txt isn't set up correctly\nMessage MrBooks36 for a fix at https://discord.com/users/1327055692179177494")
+   input('press enter to end')
+   quit()
+ repo = Repo.clone_from(repo_url, f"{p}")
+ chdir(p)
+ system(f'{p}/autobuild.bat')
+except NotADirectoryError or FileNotFoundError :
+  print("Your config.txt isn't set up correctly")
+except Exception as e: 
+  print(f'{e}\nMessage MrBooks36 for a fix at https://discord.com/users/1327055692179177494')
+input('press enter to end')
