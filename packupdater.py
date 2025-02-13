1fd03b71b8f74bc7ff4484ac7d88bc096430c644
MrBooks36 <mrbooks36@outlook.com>
2025-02-13 19:34:46 +1000

    Add files via upload

--- packupdater.py
+++ packupdater.py
@@ -8,18 +8,14 @@ from json import dump, load
 root = Tk()
 repo_url = 'https://github.com/Big-Con-Gaming/Infinite-Parkour-datapack'
 username = getlogin()
-
-
-
 root.title("Packupdater")
-# Set geometry(widthxheight)
 root.geometry('190x50')
 
 def help():
+ #get help
  root.clipboard_clear()
  root.clipboard_append('https://discord.com/users/1327055692179177494')
  messagebox.showinfo("Copied",'URL copied to Clipboard')
-
 
 def find_directories_with_name(root_dir ):
     matching_dirs = []
@@ -32,24 +28,31 @@ def find_directories_with_name(root_dir ):
 
 def run():
   try:
+   #test for custom path
    if len(txt.get()) != 0:
     data = {
     "data" : txt.get()
      } 
+    #save to JSON
     with open('config.json', 'w', encoding='utf-8') as f:
      dump(data, f, ensure_ascii=False, indent=4)
+    #set path
     p = (f"{find_directories_with_name(f'{txt.get()}/saves')}datapacks/Infinite-Parkour-datapack")
-    if path.exists(f'{txt.get()}/saves/infinite-parkour-alpha-v0.1.2/datapacks/Infinite-Parkour') :
-     rmtree(f'{txt.get()}/saves/infinite-parkour-alpha-v0.1.2/datapacks/Infinite-Parkour')
+    #find OG datapack 
+    with f"{find_directories_with_name(f'{txt.get()}/saves')}datapacks/Infinite-Parkour" as old:
+     if path.exists(old) :
+      rmtree(old)
     print(f'Folder path at\n{p}')
    else:
+    #set path
     p = (f"{find_directories_with_name(f'C:/Users/{username}/AppData/Roaming/.minecraft/saves')}/datapacks/Infinite-Parkour-datapack")
+    #find OG datapack 
+    with f"C{find_directories_with_name(f'C:/Users/{username}/AppData/Roaming/.minecraft/saves')}/datapacks/Infinite-Parkour" as old:
+     if path.exists(old):
+      rmtree(old)
     print(f'Folder path at\n{p}')
-    if path.exists(f"C{find_directories_with_name(f'C:/Users/{username}/AppData/Roaming/.minecraft/saves')}/datapacks/Infinite-Parkour"):
-     rmtree(f"{find_directories_with_name(f'C:/Users/{username}/AppData/Roaming/.minecraft/saves')}/datapacks/Infinite-Parkour")
-    print(f'Folder path at\n{p}')
-    #give access to pain in the butt to remove files 
    try:
+    #give access to pain in the butt to remove files 
     chdir(f'{p}/.git/objects/pack')
     files = listdir(f'{p}/.git/objects/pack')
     files = [f for f in files if path.isfile(f'{p}/.git/objects/pack'+'/'+f)]
@@ -58,7 +61,8 @@ def run():
     chmod(files[1], 0o777)
     chmod(files[2], 0o777)
    except:
-    print('hmmm')
+    print("pain in the butt to remove files didn't exist")
+   #remove old pack
    if path.exists(p):
      chdir('C:/')
      rmtree(p)
@@ -66,10 +70,12 @@ def run():
    else:
      print("Old datapack didn't exist. This might be a error in your path or it never existed")
      messagebox.showwarning('Warning', "Old datapack didn't exist. This might be a error in your path or it never existed")
+   #get new pack from github
    print('downloading')
    Repo.clone_from(repo_url, p)
    print('compiling')
    chdir(p)
+   #start compiler
    system(f'{p}/autobuild.bat')
    messagebox.showinfo("Done",'Done')
   except Exception as e:
@@ -77,24 +83,22 @@ def run():
    messagebox.showerror('Error', f"{e}\n An error occurred, DM MrBooks36 for help. Error copied to clipbord")
 
 def reset():
+  #reset config
   if path.exists('config.json'):
    remove('config.json')
   txt.delete(0, END)
-# inside
-def start():
- btn = Button(root, text = "Run", fg = "black", command=run)
- btn.grid(column=1, row=0)
- btn = Button(root, text = "Help", fg = "black", command=help)
- btn.grid(column=1, row=2)
-
- lbl = Label(root, text = "Custom Path")
- lbl.grid(column=2, row=0)
-
- btn = Button(root, text = "Reset Config", fg = "black", command=reset)
- btn.grid(column=2, row=2)
 
 # Execute Tkinter
-start()
+btn = Button(root, text = "Run", fg = "black", command=run)
+btn.grid(column=1, row=0)
+btn = Button(root, text = "Help", fg = "black", command=help)
+btn.grid(column=1, row=2)
+
+lbl = Label(root, text = "Custom Path")
+lbl.grid(column=2, row=0)
+
+btn = Button(root, text = "Reset Config", fg = "black", command=reset)
+btn.grid(column=2, row=2)
 txt = Entry(root, width=11)
 txt.grid(column=3, row =0)
 if path.exists('config.json'):
