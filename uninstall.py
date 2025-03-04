321dd4e8727d1963e7b2b590681c05d5f1572215
MrBooks36 <mrbooks36@outlook.com>
2025-03-04 17:31:27 +1000

    Update uninstall.py

--- uninstall.py
+++ uninstall.py
@@ -1,18 +1,20 @@
-from os import path, chdir, listdir, chmod, system, getlogin
+from os import chdir, listdir, chmod, system, getlogin,path
 from shutil import rmtree
 chdir(path.dirname(path.abspath(__file__)))
-def unlock_git_files(path):
-    git_pack_path = f"{path}/.git"
+def unlock_git_files(datapack_path):
+    git_pack_path = path.join(datapack_path, '.git', 'objects', 'pack')
     try:
         if path.exists(git_pack_path):
             chdir(git_pack_path)
             for file in listdir(git_pack_path):
                 chmod(file, 0o777)
             print("Unlocked Git files.")
+            chdir('C:/')
         else:
             print("No Git files to unlock.")
+            chdir('C:/')
     except Exception as e:
-     print(e)
+        print(e)
 unlock_git_files(path.dirname(path.abspath(__file__)))
 rmtree(chdir(path.dirname(path.abspath(__file__))))
 system(f'del C:/Users/{getlogin()}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Infinite-Parkour-datapack-updater.lnk')
