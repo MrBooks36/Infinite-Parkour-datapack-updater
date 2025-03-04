d719ed84dc83958a308c00cc8789d9c3cc5a55be
MrBooks36 <mrbooks36@outlook.com>
2025-03-04 13:34:40 +1000

    remove identity from files

--- installer compiler.bat
+++ installer compiler.bat
@@ -1,8 +1,8 @@
 @echo off
-cd C:\Users\ofk20\Documents\GitHub\Infinite-Parkour-datapack-updater
-python -m PyInstaller --noconfirm --onefile --console --icon "C:\Users\ofk20\Documents\GitHub\Infinite-Parkour-datapack-updater\pack.png"  "C:\Users\ofk20\Documents\GitHub\Infinite-Parkour-datapack-updater\updaterinstaller.py"
-move C:\Users\ofk20\Documents\GitHub\Infinite-Parkour-datapack-updater\dist\updaterinstaller.exe  C:\Users\ofk20\Documents\GitHub\Infinite-Parkour-datapack-updater
-del C:\Users\ofk20\Documents\GitHub\Infinite-Parkour-datapack-updater\updaterinstaller.spec
-rmdir /s /q C:\Users\ofk20\Documents\GitHub\Infinite-Parkour-datapack-updater\dist
-rmdir /s /q C:\Users\ofk20\Documents\GitHub\Infinite-Parkour-datapack-updater\build\packupdater\localpycs
-rmdir /s /q C:\Users\ofk20\Documents\GitHub\Infinite-Parkour-datapack-updater\build
+cd C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater
+python -m PyInstaller --noconfirm --onefile --console --icon "C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater\pack.png"  "C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater\updaterinstaller.py"
+move C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater\dist\updaterinstaller.exe  C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater
+del C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater\updaterinstaller.spec
+rmdir /s /q C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater\dist
+rmdir /s /q C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater\build\packupdater\localpycs
+rmdir /s /q C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater\build
