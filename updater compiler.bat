ecadf56bde1339dc79d3ad13556c348ccc43c4cd
MrBooks36 <mrbooks36@outlook.com>
2025-03-05 18:16:05 +1000

    add license

--- updater compiler.bat
+++ updater compiler.bat
@@ -1,8 +1,9 @@
 @echo off
 cd C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater
-python -m PyInstaller --noconfirm --onefile --windowed --icon "C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater\pack.ico"  "C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater\packupdater.py"
+python -m PyInstaller --noconfirm --onefile --windowed --version-file version.rc --icon "C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater\pack.ico"  "C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater\packupdater.py"
 move C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater\dist\packupdater.exe  C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater
 del C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater\packupdater.spec
 rmdir /s /q C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater\dist
 rmdir /s /q C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater\build\packupdater\localpycs
 rmdir /s /q C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater\build
+"C:\Program Files (x86)\Windows Kits\10\bin\10.0.26100.0\x86\signtool" sign /a /fd SHA256 C:\Users\%username%\Documents\GitHub\Infinite-Parkour-datapack-updater\updaterinstaller.exe
